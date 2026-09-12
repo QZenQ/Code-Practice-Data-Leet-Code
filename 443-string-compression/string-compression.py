40bce4991e4603babd2997fe11150aff2d05571e
Zobair hasan <103631576+QZenQ@users.noreply.github.com>
2026-09-12 14:45:46 +0600

    Time: 0 ms (100.00%) | Memory: 12.4 MB (54.57%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,43 +1,32 @@
-class Solution:
-    def compress(self, chars: List[str]) -> int:
-
-        data = [] * 2000
-        buffer = None
-        cii = -1
-        cc = 1 
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        w =0
+        count = 0
         for i in range(len(chars)):
-            print(cc)
-            if(chars[i] != buffer or i == len(chars) - 1):
+            count += 1
 
-                if(i == len(chars) - 1 and chars[i] == buffer):
-                    cc +=1
+            if(i + 1 == len(chars) or chars[i] != chars[i + 1]):
+                chars[w] = chars[i]
+                w += 1
+                if(count > 1):
+                    for digit in str(count):
+                        chars[w] = digit
+                        w+=1
 
-                if cii != -1 and cc > 1:
-                    stack = []
-                    while(cc>0):                                         
-                        stack.append(str(cc % 10))
-                        cc = cc//10
-                    while(stack):
-                        cii +=1
-                        data.insert(cii, stack.pop())
-                     
-                    
-                if(i == len(chars) - 1 and chars[i] == buffer): break 
-                cii += 1
-                buffer = chars[i]
-                data.insert(cii, chars[i])
-                cc = 1
-                    
-            else:
-                cc += 1
-                 
-                     
+                count = 0
 
-        chars[:] = data      
-        # for i in range(len(data)):
-        #     print(data[i])
+        return w                
+             
+        
+            
 
-        return len(data)            
+        
 
 
-                
+
+
+        
